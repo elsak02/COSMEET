50cd87d497b1c2fc2a3b3cdfc5e0c302f88d6d87
Oceane Leroy oceaneleroy1@gmail.com <oceaneleroy1@gmail.com>
2020-08-26 15:11:09 +0200

    added bios to the seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_105724) do
+ActiveRecord::Schema.define(version: 2020_08_26_125720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,6 +105,7 @@ ActiveRecord::Schema.define(version: 2020_08_26_105724) do
     t.float "latitude"
     t.float "longitude"
     t.string "personality_trait"
+    t.text "biography"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
