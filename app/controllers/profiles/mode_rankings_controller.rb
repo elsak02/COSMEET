7136ab247fb77c7984413b63f63a6827b327b77e
Vivian Salles <vivianmsalles@gmail.com>
2020-08-31 14:10:12 +0200

    validated form - except photos and personality traits

--- app/controllers/profiles/mode_rankings_controller.rb
+++ app/controllers/profiles/mode_rankings_controller.rb
@@ -1,10 +1,9 @@
 class Profiles::ModeRankingsController < Profiles::PreferencesController
-   def update
+  def update
     authorize current_user
-    # current_user.assign_attributes(profile_params)
-    current_user.assign_attributes(mode_ranking: params[:user][:mode_ranking].split(','))
-
-    if current_user.save
+    form = ModeRankingsForm.new(profile_params)
+    if form.valid?
+      current_user.update(profile_params)
       redirect_to users_path
     else
       render :edit
