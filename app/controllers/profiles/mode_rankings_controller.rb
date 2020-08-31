d6c001da26fe1dba7cdca5d086ce9030574992f5
Vivian Salles <vivianmsalles@gmail.com>
2020-08-31 16:01:30 +0200

    fixing bugs

--- app/controllers/profiles/mode_rankings_controller.rb
+++ app/controllers/profiles/mode_rankings_controller.rb
@@ -2,10 +2,12 @@ class Profiles::ModeRankingsController < Profiles::PreferencesController
   def update
     authorize current_user
     form = ModeRankingsForm.new(profile_params)
+    form.user = current_user
     if form.valid?
-      current_user.update(profile_params)
+      form.save
       redirect_to users_path
     else
+      flash[:alert] = form.errors.full_messages.join(", ")
       render :edit
     end
   end
--- app/controllers/profiles/photos_controller.rb
+++ app/controllers/profiles/photos_controller.rb
@@ -5,6 +5,7 @@ class Profiles::PhotosController < Profiles::PreferencesController
     if current_user.save
       redirect_to edit_profiles_relationship_types_path
     else
+      flash[:alert] = form.errors.full_messages.join(", ")
       render :edit
     end
   end
