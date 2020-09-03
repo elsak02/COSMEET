6c2f718757fc47dccf6a5f8ece4e41188ed9190c
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-09-03 16:00:05 +0200

    Swith to Swing library

--- app/javascript/plugins/init_swipe.js
+++ app/javascript/plugins/init_swipe.js
@@ -1,61 +1,28 @@
-// import Swipe from 'swipejs';
+import { Stack, Card, Direction } from 'swing';
 import { fetchWithToken } from "../plugins/fetch_with_token";
 
 const profileSwipe = () => {
-  console.log("hello");
-
-  $( document ).ready(function() {
-
-    var profiles = $(document.querySelectorAll(".user-profile-card"));
-
-    profiles.each(function() {
-
-      // listen the swipe
-      $(this).swipe({
-        swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
-
-          // var likeAction = $(this).find('.new_like input[value="Like"]')[0];
-          // var dislikeAction = $(this).find('.new_like input[value="Dislike"]')[0];
-          console.log(this)
-          const card = this[0]
-          const receiverId = card.dataset.receiverId
-          console.log(receiverId)
-          // check direction of swipe and trigger the right button
-          if (direction === "left") {
-            // dislikeAction.click();
-            postLike(receiverId, false)
-            $(this).addClass("swipe-left")
-
-          } else if (direction === "right") {
-            // likeAction.click();
-            postLike(receiverId, true)
-            $(this).addClass("swipe-right")
-          }
-        },
-        swipeStatus: swipeStatus,
-        threshold: 0,
-        fingers: `all`,
-        allowPageScroll: "vertical",
-        triggerOnTouchEnd: true
-      });
-    });
+  const cards = document.querySelectorAll(".user-profile-card")
+  console.log(Stack)
+  // console.log(Swing)
+  const stack = Stack({
+    allowedDirections: [Direction.LEFT, Direction.RIGHT]
   });
-}
+  cards.forEach((card) => {
+    // Add card element to the Stack.
+    stack.createCard(card);
+  });
 
-const swipeStatus = (event, phase, direction, distance) => {
-  if (phase == "move" && (direction == "left" || direction == "right")) {
-    var duration = 0;
-    const img_width = 597
-
-    if (direction == "left") {
-        scrollImages($(this), (img_width) + distance, duration);
-    } else if (direction == "right") {
-        scrollImages($(this), (img_width) - distance, duration);
+  stack.on('throwout', (event) => {
+    // e.target Reference to the element that has been thrown out of the stack.
+    // e.throwDirection Direction in which the element has been thrown (Direction.LEFT, Direction.RIGHT).
+    const currentCard = event.target
+    if (event.throwDirection == Direction.LEFT) {
+      postLike(currentCard.dataset.receiverId, false)
+    } else {
+      postLike(currentCard.dataset.receiverId, true)
     }
-
-  } else if (phase == "cancel") {
-    scrollImages($(this), img_width, speed);
-  }
+  });
 }
 
 const postLike = (receiver_id, liked) => {
@@ -77,12 +44,5 @@ const postLike = (receiver_id, liked) => {
     });
 }
 
-const scrollImages = (element, distance, duration) => {
-  element.css("transition-duration", (duration / 1000).toFixed(1) + "s");
-
-  //inverse the number we set in the css
-  var value = (distance < 0 ? "" : "-") + Math.abs(distance).toString();
-  element.css("transform", "translate(" + value + "px,0)");
-}
 
 export { profileSwipe }
