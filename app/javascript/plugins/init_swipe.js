2e39d2052d9c70f7bd067b096515ebd382f20233
Elsa Kiefer <elsa.kiefer@edhec.com>
2020-09-03 11:49:48 +0200

    New swiper implementation

--- app/javascript/plugins/init_swipe.js
+++ app/javascript/plugins/init_swipe.js
@@ -1,4 +1,5 @@
 // import Swipe from 'swipejs';
+import { fetchWithToken } from "../plugins/fetch_with_token";
 
 const profileSwipe = () => {
   console.log("hello");
@@ -13,15 +14,21 @@ const profileSwipe = () => {
       $(this).swipe({
         swipe:function(event, direction, distance, duration, fingerCount, fingerData) {
 
-          var likeAction = $(this).find('.new_like input[value="Like"]')[0];
-          var dislikeAction = $(this).find('.new_like input[value="Dislike"]')[0];
-
+          // var likeAction = $(this).find('.new_like input[value="Like"]')[0];
+          // var dislikeAction = $(this).find('.new_like input[value="Dislike"]')[0];
+          console.log(this)
+          const card = this[0]
+          const receiverId = card.dataset.receiverId
+          console.log(receiverId)
           // check direction of swipe and trigger the right button
           if (direction === "left") {
-            dislikeAction.click();
+            // dislikeAction.click();
+            postLike(receiverId, false)
             $(this).addClass("swipe-left")
+
           } else if (direction === "right") {
-            likeAction.click();
+            // likeAction.click();
+            postLike(receiverId, true)
             $(this).addClass("swipe-right")
           }
         },
@@ -43,6 +50,23 @@ const profileSwipe = () => {
   //   })
   // })
 
+}
+
+const postLike = (receiver_id, liked) => {
+  // Fetch "/likes"
+  // Params: { like: { receiver_id: 2, liked: true }}
+  fetchWithToken("/likes", {
+    method: "POST",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ like: { receiver_id: receiver_id, liked: liked }})
+  }).then(response => response.json())
+    .then((data) => {
+      console.log(data)
+      // handle JSON response from server
+    });
 }
 
 export { profileSwipe }
