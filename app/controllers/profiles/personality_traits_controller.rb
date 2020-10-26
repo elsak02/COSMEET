8ce80e1e68f22ab3295de2b1d89547451089ff59
Vivian Salles <vivianmsalles@gmail.com>
2020-10-26 15:35:09 +0100

    deleted raise

--- app/controllers/profiles/personality_traits_controller.rb
+++ app/controllers/profiles/personality_traits_controller.rb
@@ -1,7 +1,6 @@
 class Profiles::PersonalityTraitsController < Profiles::PreferencesController
   def update
     authorize current_user
-    raise
     current_user.assign_attributes(profile_params)
     if current_user.save
       redirect_to edit_profiles_activities_path
